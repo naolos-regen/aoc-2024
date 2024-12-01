ea84b8ab577f8ad2713a5d283257d6da2b196b5b
naolos-regen <naolos.regen@proton.me>
2024-12-01 13:30:33 +0100

    .gitignore??

--- 00_day/Historian_Hysteria/target/debug/Historian_Hysteria.d
+++
@@ -1,1 +0,0 @@
-/home/naolos/Documents/Programming/Advent-of-code/00_day/Historian_Hysteria/target/debug/Historian_Hysteria: /home/naolos/Documents/Programming/Advent-of-code/00_day/Historian_Hysteria/src/main.rs
--- 00_day/Historian_Hysteria/target/debug/deps/Historian_Hysteria-10e2243ceef5484b.d
+++
@@ -1,5 +0,0 @@
-/home/naolos/Documents/Programming/Advent-of-code/00_day/Historian_Hysteria/target/debug/deps/libHistorian_Hysteria-10e2243ceef5484b.rmeta: src/main.rs
-
-/home/naolos/Documents/Programming/Advent-of-code/00_day/Historian_Hysteria/target/debug/deps/Historian_Hysteria-10e2243ceef5484b.d: src/main.rs
-
-src/main.rs:
